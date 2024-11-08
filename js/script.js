9d0be273137eb59f55c09785547ba676590fb6da
Agustin <agustinruiz3007@gmail.com>
2024-11-08 16:29:56 -0300

    Update time text content

--- js/script.js
+++ js/script.js
@@ -3,15 +3,15 @@ const minutes = document.querySelector("#minutes");
 const seconds = document.querySelector("#seconds");
 
 function showTime() {
-  const date = new Date()
-  let hour = date.getHours()
-  let min = date.getMinutes()
-  let sec = date.getSeconds()
-  hours.textContent = String(hour)
-  minutes.textContent = String(min)
-  seconds.textContent = String(sec)
+	const date = new Date();
+	let hour = date.getHours();
+	let min = date.getMinutes();
+	let sec = date.getSeconds();
+	hours.textContent = String(hour).padStart(2, "0");
+	minutes.textContent = String(min).padStart(2, "0");
+	seconds.textContent = String(sec).padStart(2, "0");
 }
 
-setInterval(showTime, 1000)
+setInterval(showTime, 1000);
 
-showTime()
+showTime();
